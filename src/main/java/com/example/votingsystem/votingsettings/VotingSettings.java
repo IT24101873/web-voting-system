749157ec83f0af8592d85e552697e7a628c596fe
jesmeen <IT24101873@my.sliit.lk>
2025-09-25 03:00:01 +0530

    Add vote settings entity

--- src/main/java/com/example/votingsystem/votingsettings/VotingSettings.java
+++ src/main/java/com/example/votingsystem/votingsettings/VotingSettings.java
@@ -1,4 +1,20 @@
-package com.example.votingsystem.votingsettings;
+package com.example.votingsystem.votingSettings;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "VOTING_SETTINGS")
 public class VotingSettings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "start_date")
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date")
+    private LocalDateTime endDate;
 }
